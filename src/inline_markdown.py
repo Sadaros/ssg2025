9f4507ff9cbb71b78e0be012a5c7ad5eaba8b701
sadaros <post@chrislearns.tech>
2025-05-18 01:47:35 +0200

    added extraction functions for links and images in markdown

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,3 +1,4 @@
+import re
 from typing import Sequence
 
 from textnode import TextNode, TextType
@@ -27,3 +28,11 @@ def split_nodes_delimiter(
                 sub_list.append(TextNode(word, text_type))
         new_nodes.extend(sub_list)
     return new_nodes
+
+
+def extract_markdown_images(text: str) -> list[tuple]:
+    return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+
+
+def extract_markdown_links(text: str) -> list[tuple]:
+    return re.findall(r"\[(.*?)\]\((.*?)\)", text)
